05d2a36fb7d8eee11d85b6c05be907f9e2ddd159
Evhenii Halenko <Re4port@ukr.net>
2017-12-25 13:14:34 +0200

    add tests for Auth::Session::Contract::Create

--- spec/concepts/auth/session/contract/create_spec.rb
+++ spec/concepts/auth/session/contract/create_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Auth::Session::Contract::Create do
+  subject { described_class.new(user) }
+  let(:missed_params) { Hash.new }
+  let(:valid_params) { { username: user.username, password: user.password } }
+  let(:invalid_params) { { username: user.username, password: 'fake password' } }
+  let!(:user) { FactoryGirl.create(:user) }
 
+  describe 'validations' do
+    it 'success' do
+      expect(subject.validate(valid_params)).to eq true
+    end
+
+    it 'fail on empty fields' do
+      expect(subject.validate(missed_params)).to eq false
+      subject.errors[:username].include? 'must be filled'
+      subject.errors[:password].include? 'must be filled'
+    end
+
+    it 'fail on fake password' do
+      expect(subject.validate(invalid_params)).to eq false
+      subject.errors[:username].include? 'invalid credentials'
+    end
+  end
 end
