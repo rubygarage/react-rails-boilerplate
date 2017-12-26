642b2458513d94b8b30a1715592c3b4ea7665579
Evhenii Halenko <Re4port@ukr.net>
2017-12-26 11:24:10 +0200

    extend and fix Auth::Registration::Create tests

--- spec/concepts/auth/registration/operation/create_spec.rb
+++ spec/concepts/auth/registration/operation/create_spec.rb
@@ -15,6 +15,12 @@ RSpec.describe Auth::Registration::Create do
         }
       }
 
+      before do
+        confirmation = double('email confirmation')
+        allow(confirmation).to receive(:deliver_later)
+        allow(ConfirmationMailer).to receive(:confirmation_email) { confirmation }
+      end
+
       it 'setup user' do
         expect(subject['model'].username).to eq user.username
         expect(subject['model'].email).to eq user.email
@@ -25,7 +31,33 @@ RSpec.describe Auth::Registration::Create do
       end
 
       it 'send confirmation email' do
-        expect(ConfirmationMailer).to receive(:confirmation_email).and_call_original.once
+        expect(ConfirmationMailer).to receive_message_chain(:confirmation_email, :deliver_later)
+        subject
+      end
+    end
+
+    context 'invalid params' do
+      let(:params) { { username: user.username, password: user.password, email: user.email } }
+
+      it 'is not setup user' do
+        expect(subject['model'].username).to be_nil
+        expect(subject['model'].email).to be_nil
+      end
+
+      it 'show errors' do
+        expect(subject['result.contract.default'].errors[:password_confirmation]).to include "doesn't match"
+      end
+
+      it 'fail validation' do
+        expect(subject).to be_failure
+      end
+
+      it 'not to persist user' do
+        expect(subject['model']).not_to be_persisted
+      end
+
+      it 'is not send confirmation email' do
+        expect(ConfirmationMailer).not_to receive(:confirmation_email)
         subject
       end
     end
