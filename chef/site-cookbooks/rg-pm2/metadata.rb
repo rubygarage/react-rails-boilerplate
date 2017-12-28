ae2fead019a28d123793bb3ca05320c151ec70e5
Anton Vukolov <vatnajokull89@gmail.com>
2017-12-28 10:05:58 +0200

    first attempt for adding pm2's monit config

---
+++ chef/site-cookbooks/rg-pm2/metadata.rb
@@ -0,0 +1,11 @@
+name             'rg-pm2'
+maintainer       'Anton Vukolov'
+maintainer_email 'vatnajokull89@gmail.com'
+license          'All rights reserved'
+description      'PM2 configuration'
+long_description 'PM2 configuration'
+version          '0.1.0'
+
+depends 'rg-attributes'
+
+depends 'pm2'
--- webpack/pm2-app.config.js
+++ webpack/pm2-app.config.js
@@ -1,11 +1,17 @@
+const path = require('path');
+
 module.exports = {
   apps: [
     {
       name: 'rg-react-redux-isomorphic-example',
-      script: 'bin/server.js',
-      pid_file: '../tmp/pids/rg-react-redux-isomorphic-example-node.pid',
-      out_file: '../log/rg-react-redux-isomorphic-example-node-out.log',
-      error_file: '../log/rg-react-redux-isomorphic-example-node-err.log',
+      // script: __dirname + 'bin/server.js',
+      // pid_file: '../tmp/pids/rg-react-redux-isomorphic-example-node.pid',
+      // out_file: '../log/rg-react-redux-isomorphic-example-node-out.log',
+      // error_file: '../log/rg-react-redux-isomorphic-example-node-err.log',
+      script: __dirname + '/bin/server.js',
+      pid_file: path.resolve(__dirname, '../pids/rg-react-redux-isomorphic-example-node.pid'),
+      out_file: path.resolve(__dirname, '../log/rg-react-redux-isomorphic-example-node-out.log'),
+      error_file: path.resolve(__dirname, '../log/rg-react-redux-isomorphic-example-node-err.log'),
       merge_logs: true,
       combine_logs: true,
       env_development: {
