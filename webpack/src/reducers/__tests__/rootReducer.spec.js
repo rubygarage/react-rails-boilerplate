465a3c792e79b1fb3f63b6a089019ef20f8f4eb5
Evhenii Halenko <Re4port@ukr.net>
2017-12-29 18:01:12 +0200

    normalize rootReducer.spec

--- webpack/src/reducers/__tests__/rootReducer.spec.js
+++ webpack/src/reducers/__tests__/rootReducer.spec.js
@@ -7,30 +7,10 @@ describe('rootReducer', () => {
   const state = createStore(reducer).getState()
 
   describe('can handle SIGN_OUT_SUCCESS', () => {
-    const entities = { entities: { cards: { 1: { id: '1' }, 2: { id: '2' }, 3: { id: '3' } } } }
-    const stateWithEntities = { ...state, ...entities }
+    const stateWithEntities = { ...state }
 
     it('resets state', () => {
       expect(rootReducer(stateWithEntities, { type: 'SIGN_OUT_SUCCESS' })).toEqual({ ...state })
-    })
-  })
-
-  describe('can handle CLEAR_ALL_CARD_LISTS', () => {
-    const cards = { 1: { id: '1' }, 2: { id: '2' }, 3: { id: '3' } }
-
-    const stateWithCards = {
-      entities: { cards }
-    }
-
-    const expectedState = {
-      ...state,
-      entities: { cards: {} }
-    }
-
-    it('clears entitie cards', () => {
-      expect(rootReducer({ ...state, ...stateWithCards }, { type: 'CLEAR_ALL_CARD_LISTS' })).toEqual({
-        ...expectedState
-      })
     })
   })
 })
