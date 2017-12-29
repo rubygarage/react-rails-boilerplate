d949d69662bac9398115b2c48a11545b3d95996c
Aleks <aleksandr.ihnatkin@rubygarage.org>
2017-12-29 10:09:01 +0200

    Feature/add enzyme to react (#10)
    * tests for actions in react
    
    finish test for all actions
    
    corrected test
    
    * fix it comments ('sign out', 'creates an action to get user')

---
+++ webpack/src/actions/__tests__/signin.spec.js
@@ -0,0 +1,9 @@
+import { signIn } from 'actions/signin'
+
+describe('sign_in', () => {
+  it('creates an action to sign in', () =>{
+    const expectedAction = { type: 'SIGN_IN_REQUEST' }
+
+    expect(signIn()).toEqual(expectedAction)
+  })
+})
--- webpack/src/constants/actions.js
+++ webpack/src/constants/actions.js
@@ -6,6 +6,7 @@ export const VALIDATE_TOKEN = 'VALIDATE_TOKEN'
 export const SIGN_UP = 'SIGN_UP'
 export const SIGN_IN = 'SIGN_IN'
 export const SIGN_OUT = 'SIGN_OUT'
+export const OAUTH = 'OAUTH'
 
 export const SEND_RESTORE_PASSWORD_EMAIL = 'SEND_RESTORE_PASSWORD_EMAIL'
 export const UPDATE_PASSWORD = 'UPDATE_PASSWORD'
