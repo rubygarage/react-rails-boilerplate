1e4354111e18ddfa2742d45e5d485ae818945899
Evhenii Halenko <Re4port@ukr.net>
2018-01-02 14:13:48 +0200

    add tests for SignUp reducer

--- webpack/src/reducers/__tests__/signin.spec.js
+++ webpack/src/reducers/__tests__/signin.spec.js
@@ -3,29 +3,29 @@ import signin from 'reducers/signin'
 describe('Signin reducer', () => {
   it('has an loading as initial state', () => {
     expect(signin(undefined, { type: 'unexpected' })).toEqual({
-    	currentUser: {},
-			loading: false
+      currentUser: {},
+      loading: false
     })
   })
 
   it('sets state loading into true', () => {
     expect(signin(undefined, { type: 'SIGN_IN_REQUEST' })).toEqual({
-    	currentUser: {},
-			loading: true
+      currentUser: {},
+      loading: true
     })
   })
 
   it('sets user and state', () => {
     expect(signin({ currentUser: { id: 1 } }, { type: 'SIGN_IN_SUCCESS', currentUser: { id: 3 } })).toEqual({
-    	currentUser: { id: 3 },
-			loading: false
+      currentUser: { id: 3 },
+      loading: false
     })
   })
 
   it('sets state loading into false', () => {
     expect(signin(undefined, { type: 'SIGN_IN_ERROR' })).toEqual({
-    	currentUser: {},
-			loading: false
+      currentUser: {},
+      loading: false
     })
   })
 })
