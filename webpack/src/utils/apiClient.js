aaa7a49d8a271086c0131ac414b0ad8fcc2ac864
Denis Kostyrko <irdis.evale@gmail.com>
2018-01-04 11:24:38 +0200

    Add eslint

--- webpack/src/utils/apiClient.js
+++ webpack/src/utils/apiClient.js
@@ -12,26 +12,26 @@ export default class ApiClient {
   }
 
   // private
-  _buildHeaders(req) {
+  static _buildHeaders(req) {
     const authToken = req ? this._getAuthTokenFromServerRequest(req) : this._getHeadersFromClientCookies()
     const headers = authToken ? { authorization: authToken } : {}
 
     return headers
   }
 
-  _getAuthTokenFromServerRequest(req) {
+  static _getAuthTokenFromServerRequest(req) {
     const cookies = new Cookies(req.headers.cookie)
 
     return cookies.get('authToken')
   }
 
-  _getHeadersFromClientCookies() {
+  static _getHeadersFromClientCookies() {
     const cookies = new Cookies()
 
     return cookies.get('authToken')
   }
 
-  _buildAxiosInstance(authHeaders) {
+  static _buildAxiosInstance(authHeaders) {
     const options = { headers: authHeaders }
     if (isServer()) { options.baseURL = config.apiBaseUrl }
     const client = axios.create(options)
