55efe6debf4ecc8e6b7bc86796ee5765323c7a0e
Evhenii Halenko <Re4port@ukr.net>
2018-01-15 14:17:15 +0200

    fix eslint errors

--- webpack/src/sagas/oauth.js
+++ webpack/src/sagas/oauth.js
@@ -1,30 +1,30 @@
-import { normalize } from 'normalize-json-api'
-import { takeEvery, call, put } from 'redux-saga/effects'
-import { SIGN_IN, OAUTH, REQUEST, SUCCESS, ERROR } from 'constants/actions'
-import { setTokenToStorage } from 'utils/tokens'
-import { openPopup } from 'utils/popup'
-import { redirect } from 'helpers/redirect'
+import { normalize } from 'normalize-json-api';
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { SIGN_IN, OAUTH, REQUEST, SUCCESS, ERROR } from 'constants/actions';
+import { setTokenToStorage } from 'utils/tokens';
+import openPopup from 'utils/popup';
+import { redirect } from 'helpers/redirect';
 
 export function* auth(response) {
   if (response.newUser === 'true') {
-    yield call(redirect, '/sign_up')
+    yield call(redirect, '/sign_up');
   } else {
-    yield call(setTokenToStorage, response.headers)
-    const { entities, results } = yield call(normalize, response.userData)
-    yield put({ type: SIGN_IN + SUCCESS, entities, currentUser: results })
-    yield call(redirect, '/')
+    yield call(setTokenToStorage, response.headers);
+    const { entities, results } = yield call(normalize, response.userData);
+    yield put({ type: SIGN_IN + SUCCESS, entities, currentUser: results });
+    yield call(redirect, '/');
   }
 }
 
 export function* signIn({ provider }) {
   try {
-    const response = yield call(openPopup, `/omniauth/${provider}`, provider)
-    yield call(auth, response)
+    const response = yield call(openPopup, `/omniauth/${provider}`, provider);
+    yield call(auth, response);
   } catch (error) {
-    yield put({ type: SIGN_IN + ERROR, error })
+    yield put({ type: SIGN_IN + ERROR, error });
   }
 }
 
 export default function* watchOauth() {
-  yield takeEvery(OAUTH + REQUEST, signIn)
+  yield takeEvery(OAUTH + REQUEST, signIn);
 }
