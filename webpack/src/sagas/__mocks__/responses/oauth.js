7437b943e2ef733cb8f4a9df82b5ae83001a1ed5
Evhenii Halenko <Re4port@ukr.net>
2018-01-17 17:59:54 +0200

    add tests for sagas and reducers, fix linter errors

---
+++ webpack/src/sagas/__mocks__/responses/oauth.js
@@ -0,0 +1,19 @@
+const oauthResponse = {
+  data: {
+    id: '17',
+    type: 'users',
+    attributes: {
+      email: 're4port@ukr.net',
+      username: 'Евгений Галенко',
+      provider: 'facebook',
+      uid: '1720825541301392',
+    },
+    relationships: {
+      avatar: {
+        data: null,
+      },
+    },
+  },
+};
+
+export default oauthResponse;
