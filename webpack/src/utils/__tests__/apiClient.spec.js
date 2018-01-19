5868176057382990e42528d50fa8bc044f11c412
polykoi <lambwithastick@gmail.com>
2018-01-19 18:49:48 +0200

    Feature/tests for utils (#29)
    * tests for utils/tokens
    
    * tests for utils: validation, apiClient
    
    * fixes due to review

---
+++ webpack/src/utils/__tests__/apiClient.spec.js
@@ -0,0 +1,37 @@
+import ApiClient from 'utils/apiClient'
+import config from 'utils/config';
+
+jest.mock('universal-cookie', () => (
+  function (params = 'ClientAuthToken') {
+    return {
+      get: () => (params),
+    };
+  }
+));
+
+const mockIsServer = jest.fn();
+jest.mock('helpers/server', () => ({ isServer: () => mockIsServer() }));
+
+const req = {
+  headers: {
+    cookie: 'RequestAuthToken',
+  }
+};
+
+describe('ApiClient#buildClient()', () => {
+  it('creates api client with headers from client cookie when passed no params', () => {
+    const client = new ApiClient().buildClient();
+    expect(client.defaults.headers.authorization).toBe('ClientAuthToken');
+  });
+
+  it('creates api client with headers from request when it passed to params', () => {
+    const client = new ApiClient().buildClient(req);
+    expect(client.defaults.headers.authorization).toBe('RequestAuthToken');
+  });
+
+  it('sets baseURL if run on server', () => {
+    mockIsServer.mockReturnValueOnce(true);
+    const client = new ApiClient().buildClient(req);
+    expect(client.defaults.baseURL).toBe(config.apiBaseUrl);
+  });
+});
