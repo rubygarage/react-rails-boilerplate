3f8e4dc8c51f308d42e6236b4aae0c28bbd3c498
Evhenii Halenko <Re4port@ukr.net>
2018-01-22 17:29:02 +0200

    done

--- webpack/src/utils/googleAnalytics.js
+++ webpack/src/utils/googleAnalytics.js
@@ -5,12 +5,10 @@ export default class GoogleAnalytics {
   // react-ga is a singleton, so initialize is only need to be called once.
   // consequent calls won't do anything
   constructor() {
-    console.log('__GoogleAnalytics constructor triggered__!');
     ReactGA.initialize(config.googleAnalyticsUID);
   }
 
   triggerPageView = () => {
-    console.log('triggerPageView was triggered _____________________!');
     ReactGA.set({ page: window.location.pathname + window.location.search });
     ReactGA.pageview(window.location.pathname + window.location.search);
   }
