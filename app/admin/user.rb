f6967f2d8c44dbc7c74968f0ab42911cb0be7374
Aleks <aleksandr.ihnatkin@rubygarage.org>
2018-01-24 17:24:11 +0200

    add show avatar to view user info in admin path (#31)
    change message without avatar
    
    fix comments
    
    add preview avatar before to upload in edit user
    
    clear comment

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -24,7 +24,7 @@ ActiveAdmin.register Admin::User, as: "User" do
     end
 
     f.has_many :avatar, class: 'has_one', allow_destroy: true do |avatar|
-      avatar.input :image, as: :file, :hint => f.object.avatar.present? \
+      avatar.input :image, as: :file, hint: f.object.avatar.present? \
         ? image_tag(f.object.avatar.image_url(:thumb))
         : content_tag(:span, I18n.t('active_admin.user.without_avatar'))
     end
@@ -51,4 +51,23 @@ ActiveAdmin.register Admin::User, as: "User" do
   action_item :unconfirm_email, only: %i[show edit], if: proc { resource.confirmed_at? } do
     link_to I18n.t('active_admin.user.unconfirm_email'), unconfirm_email_admin_user_path(resource), method: :post
   end
+
+  show do
+    panel I18n.t('active_admin.user.user_details') do
+      attributes_table_for user do
+        row :username
+        row :email
+        row :password_digest
+        row :confirmed_at
+        row :created_at
+        row :updated_at
+        row :avatar do |img|
+          img.avatar.present? \
+            ? image_tag(img.avatar.image_url(:thumb), size: '80x60')
+            : content_tag(:span, I18n.t('active_admin.user.without_avatar'))
+        end
+      end
+    end
+  end
+
 end
