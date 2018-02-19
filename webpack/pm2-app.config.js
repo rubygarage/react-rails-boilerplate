66caae18476050eb53d8a9eb19ff9e643d1c797e
Nesterenko Sergey <serhii.nesterenko@rubygarage.org>
2018-02-19 10:32:25 +0200

    fixed nginx config for front-end, added npm command for build and start production, using it in pm2 config

--- webpack/pm2-app.config.js
+++ webpack/pm2-app.config.js
@@ -2,7 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'boilerplate-node-client',
-      script: 'bin/server.js',
+      // script: 'bin/server.js',
+      script: 'npm',
+      args: 'build-start-prod',
       pid_file: '../tmp/pids/boilerplate-node.pid',
       out_file: '../log/boilerplate-node-out.log',
       error_file: '../log/boilerplate-node-err.log',
