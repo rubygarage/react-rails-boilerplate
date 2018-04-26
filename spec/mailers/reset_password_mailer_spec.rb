61bd0f4968d56c87a25c49167d2f9926c9267ff4
Eugene Poberezhny <yevhenii.poberezhnyi@rubygarage.org>
2018-04-26 14:19:45 +0300

    update reset password mailer spec

--- spec/mailers/reset_password_mailer_spec.rb
+++ spec/mailers/reset_password_mailer_spec.rb
@@ -2,20 +2,16 @@ require 'rails_helper'
 
 RSpec.describe ResetPasswordMailer, type: :mailer do
   describe 'reset instructions' do
+    subject { ResetPasswordMailer.reset_instructions(user) }
+
     let(:user) { create(:user) }
-    let(:mail) { ResetPasswordMailer.reset_instructions(user) }
     let(:token) { FFaker::Guid.guid }
 
-    before do
-      allow(Auth::Token::ResetPassword).to receive(:generate).and_return(token)
-    end
+    it 'renders the headers and token' do
+      expect(Auth::Token::ResetPassword).to receive(:generate) { token }
 
-    it 'renders the headers' do
-      expect(mail.to).to eq([user.email])
-    end
-
-    it 'renders the token' do
-      expect(mail.body.encoded).to include(token)
+      expect(subject.to).to eq([user.email])
+      expect(subject.body.encoded).to include(token)
     end
   end
 end
